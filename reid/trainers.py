7777b7ddcdd26f606ceea592aa39639fb76fb24b
Shengcai Liao <50141276+ShengcaiLiao@users.noreply.github.com>
2020-12-22 09:20:38 +0400

    Fix a potential bug in training
    Line 17 in trainers.py

--- reid/trainers.py
+++ reid/trainers.py
@@ -14,6 +14,7 @@ class BaseTrainer(object):
 
     def train(self, epoch, data_loader, optimizer):
         self.model.train()
+        self.criterion.train()
 
         batch_time = AverageMeter()
         data_time = AverageMeter()
